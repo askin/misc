51a8338ce1729e458d1ea6beba8efdc0fea630ce
Aşkın Yollu <askin@askin.ws>
2010-04-02 03:42:45 +0300

    fix indent and dos to unix

--- templates/kelebek/404.php
+++ templates/kelebek/404.php
@@ -1,18 +1,13 @@
 <?php get_header(); ?>
 <!-- Container -->
 <div class="CON">
-
-<!-- Start SC -->
-<div class="SC">
-
-<h2 class="pagetitle">Hata 404</h2>
-<p> Sayfa Bulunamadı<a href="http://www.domatessuyu.com">.</a></p>
-
-</div>
-<!-- End SC -->
-
-<?php get_sidebar(); ?>
+    <!-- Start SC -->
+    <div class="SC">
+        <h2 class="pagetitle">Hata 404</h2>
+        <p> Sayfa Bulunamadı<a href="http://blog.yollu.com">.</a></p>
+    </div>
+    <!-- End SC -->
+    <?php get_sidebar(); ?>
 </div>
 <!-- End CON -->
-
 <?php get_footer(); ?>
